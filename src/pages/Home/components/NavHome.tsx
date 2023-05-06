3eb2d46fb96ad64a210abc7966502bd28a04c0be
Pedro Veras <veraspedro1608@gmail.com>
2023-05-06 15:20:22 -0300

    Feat: Navbar Home

---
+++ src/pages/Home/components/NavHome.tsx
@@ -0,0 +1,27 @@
+import './NavHome.css'
+
+import LogoNavbarHome from '../../../assets/logo-ignite-coffee.png'
+
+import { MapPin, ShoppingCart } from "@phosphor-icons/react";
+
+export default function NavHome() {
+    return (
+        <div className="containerNavHome">
+            <div className="navHome">
+                <div className="logoNavHome">
+                    <img src={LogoNavbarHome} alt="" />
+                </div>
+
+                <div className="loc-comprar">
+                    <button className='buttonLocNavbar'>
+                        <MapPin size={20} color="#8047F8" weight="fill" />
+                        <p>Anápolis, GO</p>
+                    </button>
+                    <button className='buttonBuyNavbar'>
+                        <ShoppingCart size={24} color="#C47F17" weight="fill" />
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import NavHome from "./components/NavHome";
+
+export default function Home() {
+    return (
+        <div className="containerNavHome">
+            <NavHome />
+        </div>
+    )
+}
