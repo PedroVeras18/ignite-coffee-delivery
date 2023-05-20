0bc87b43add84a59cc2dac9f16dcb11c0e54f9ce
Pedro Veras <veraspedro1608@gmail.com>
2023-05-20 20:08:41 -0300

    feat: :sparkles: style list card coffee

--- src/components/CardCoffee/CardCoffee.tsx
+++ src/components/CardCoffee/CardCoffee.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import './CardCoffee.css'
 
 import { ICoffee } from "../../@types/coffee"
+import { Minus, Plus, ShoppingCartSimple } from '@phosphor-icons/react'
 
 type ICardCoffeeProps = {
     data: ICoffee
@@ -10,14 +11,22 @@ type ICardCoffeeProps = {
 export default function CardCoffee({ data }: ICardCoffeeProps) {
     const [countQuantity, setCountQuantity] = useState<number>(0)
 
-    return(
+    function handleCount(action: 'inc' | 'dec') { 
+        if (action === 'inc') {
+            setCountQuantity((prev) => prev + 1)
+        } else if (action === 'dec' && countQuantity > 0) {
+            setCountQuantity((prev) => prev - 1)
+        }
+    }
+
+    return (
         <div className='containerCardCoffee'>
             <div className="imageCardCoffee">
                 <img src={data.srcImg} alt="" />
             </div>
             <div className="tagsCardCoffee">
                 {data.tags.map((tag) => (
-                    <p>{tag}</p>
+                    <p>{tag.toLocaleUpperCase()}</p>
                 ))}
             </div>
             <div className="nameCardCoffee">
@@ -33,8 +42,25 @@ export default function CardCoffee({ data }: ICardCoffeeProps) {
                         <strong>{data.price}</strong>
                     </p>
                 </div>
-                <div className="countCardCoffee">
-                    <p>- 1 +</p>
+                <div className="countBuyCardCoffee">
+                    <div className="countCardCoffee">
+                        <Minus
+                            size={18}
+                            color="#8047F8"
+                            weight="bold"
+                            onClick={() => handleCount('dec')}
+                        />
+                        <p>{countQuantity}</p>
+                        <Plus
+                            size={18}
+                            color="#8047F8"
+                            weight="bold"
+                            onClick={() => handleCount('inc')}
+                        />
+                    </div>
+                    <div className="buyCardCoffee">
+                        <ShoppingCartSimple size={22} color="#ffffff" weight="fill" />
+                    </div>
                 </div>
             </div>
         </div>
