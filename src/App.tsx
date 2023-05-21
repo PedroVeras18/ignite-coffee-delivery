135365c7d818ebb48da37285cb51ec5b8acca097
Pedro Veras <veraspedro1608@gmail.com>
2023-05-21 17:45:25 -0300

    feat: :sparkles: implementing routes App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
-import Home from './pages/Home'
+
+import { Routes } from './routes'
 
 function App() {
   return (
     <div className='App'>
-      <Home />
+      <Routes />
     </div>
   )
 }
